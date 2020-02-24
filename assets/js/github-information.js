c5188d03518b0ff9e592978add51ed2c5516ee25
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-02-24 14:55:21 +0000

    fix bug so repo list is displayed on page load

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 
 }
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -71,4 +73,4 @@ function fetchGitHubInformation(event) {
         });
 }
 
-
+$(document).ready(fetchGitHubInformation);
